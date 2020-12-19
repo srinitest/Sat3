bc5df87e5d6017fbd9a18dc57b2d53d97f0d2bb0
srinitest <yuvaraghavan87@gmail.com>
2020-12-19 13:29:18 +0530

    updated

---
+++ src/org/tst/Saturday.java
@@ -0,0 +1,19 @@
+package org.tst;
+
+public class Saturday {
+	
+	
+	public void sam() {
+
+		System.out.println("hhjkj");
+		System.out.println("hhjkfghfj");
+	}
+	public static void main(String[] args) {
+		Saturday s=new Saturday();
+		s.sam();
+	}
+
+	}
+
+
+
